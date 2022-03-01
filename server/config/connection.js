4249132b255bdd0049a0842dcbec26a4a93ede5f
Siddy <dancore99@outlook.com>
2022-03-01 15:46:28 +0800

    Deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/jobapp", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    "mongodb+srv://Daniel:Sixarks2017@cluster0.pbrpv.mongodb.net/jobapp?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 module.exports = mongoose.connection;
