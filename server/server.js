9a919f5dbd5120bd0799b9997c86a11e814b6303
Daniel Sixsmith <86714782+SiddySixsmith@users.noreply.github.com>
2022-03-02 09:11:32 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -15,6 +15,7 @@ async function startServer() {
     typeDefs,
     resolvers,
     context: authMiddleware,
+    cors: true
   });
   await apolloServer.start();
   apolloServer.applyMiddleware({ app });
