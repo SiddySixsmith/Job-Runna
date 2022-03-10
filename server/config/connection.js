6293244c6d65c05f7da72c455f2ba2ec8a7bc1cc
Siddy <dancore99@outlook.com>
2022-03-10 21:13:37 +0800

    production setup

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,15 +1,15 @@
 const mongoose = require("mongoose");
 
 // development connection
-mongoose.connect("mongodb://localhost:27017/jobapptest", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-// production connection
-// mongoose.connect(process.env.MONGODB_URI, {
+// mongoose.connect("mongodb://localhost:27017/jobapptest", {
 //   useNewUrlParser: true,
 //   useUnifiedTopology: true,
 // });
+
+// production connection
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 module.exports = mongoose.connection;
