8f3cebc3aec453a28c9eb7e3af326612bfba3291
Siddy <dancore99@outlook.com>
2022-03-10 21:00:18 +0800

    service worker fix-no longer fully installable

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,15 @@ import "./styles/index.css";
 import App from "./App";
 import StyledEngineProvider from "@mui/material/StyledEngineProvider";
 import reportWebVitals from "./reportWebVitals";
-import * as serviceWorker from "./serviceworker";
+import { Workbox } from "workbox-window";
+
+if ("serviceWorker" in navigator) {
+  // Register workbox service worker
+  const workboxSW = new Workbox("/src-sw.js");
+  workboxSW.register();
+} else {
+  console.error("Service workers are not supported in this browser.");
+}
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,4 +23,4 @@ ReactDOM.render(
   document.getElementById("root")
 );
 reportWebVitals();
-serviceWorker.register();
+// serviceWorker.register();
